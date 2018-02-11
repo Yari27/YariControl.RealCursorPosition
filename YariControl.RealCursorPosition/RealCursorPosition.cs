5a1e04925a03213e2c7e72dfaa37877c7f2d2486
Jarosław Pietras <jarko2227@gmail.com>
2018-02-11 17:36:12 +0100

    some fix and improve code

--- YariControl.RealCursorPosition/RealCursorPosition.cs
+++ YariControl.RealCursorPosition/RealCursorPosition.cs
@@ -5,16 +5,17 @@ using System.Windows.Forms;
 
 namespace YariControl.RealCursorPosition
 {
+
     public static class ScreenPixelColor
     {
         [DllImport("user32.dll")]
-        public static extern IntPtr GetDC(IntPtr hWnd);
+        private static extern IntPtr GetDC(IntPtr hWnd);
 
         [DllImport("gdi32.dll")]
-        public static extern uint GetPixel(IntPtr hdc, int nXPos, int nYPos);
+        private static extern uint GetPixel(IntPtr hdc, int nXPos, int nYPos);
 
         [DllImport("user32.dll")]
-        public static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+        private static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
 
         public static Color GetPixelColor(int x, int y)
         {
@@ -23,6 +24,7 @@ namespace YariControl.RealCursorPosition
             ReleaseDC(IntPtr.Zero, hdc);
             return Color.FromArgb((int)(pixel & 0x000000FF), (int)(pixel & 0x0000FF00) >> 8, (int)(pixel & 0x00FF0000) >> 16);
         }
+
         public static Color GetPixelColor(Point position)
         {
             return GetPixelColor(position.X, position.Y);
@@ -37,10 +39,10 @@ namespace YariControl.RealCursorPosition
         }
     }
 
-    public static class DisplayTools
+    public static class DisplayScreenTools
     {
         [DllImport("gdi32.dll")]
-        static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
+        private static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
 
         private enum DeviceCap
         {
@@ -48,42 +50,60 @@ namespace YariControl.RealCursorPosition
             Desktophorzres = 118
         }
 
-        public static Size GetPhysicalDisplaySize()
+        public static Size GetPhysicalDisplaySize
         {
-            Graphics g = Graphics.FromHwnd(IntPtr.Zero);
-            IntPtr desktop = g.GetHdc();
+            get {
+                Graphics g = Graphics.FromHwnd(IntPtr.Zero);
+                IntPtr desktop = g.GetHdc();
+                
+                int physicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.Desktopvertres);
+                int physicalScreenWidth = GetDeviceCaps(desktop, (int)DeviceCap.Desktophorzres);
 
-            int physicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.Desktopvertres);
-            int physicalScreenWidth = GetDeviceCaps(desktop, (int)DeviceCap.Desktophorzres);
-
-            return new Size(physicalScreenWidth, physicalScreenHeight);
+                return new Size(physicalScreenWidth, physicalScreenHeight);
+            }
         }
 
-        public static System.Drawing.Size GetVirtualDisplaySize()
+        public static Size GetVirtualDisplaySize
         {
-            return new Size(SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height);
+            get{
+                return new Size(SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height);
+            }
         }
 
-        public static int GetFontZoom()
+        public static Point GetFontZoomProcentage
         {
-            Size resolution = GetPhysicalDisplaySize();
-            Size virtualscreen = GetVirtualDisplaySize();
-            int zoomH = (resolution.Height * 100) / virtualscreen.Height;
-            int zoomW = (resolution.Width * 100) / virtualscreen.Width;
+            get {
+                Size resolution = GetPhysicalDisplaySize;
+                Size virtualscreen = GetVirtualDisplaySize;
+                int zoomH = (resolution.Height * 100) / virtualscreen.Height;
+                int zoomW = (resolution.Width * 100) / virtualscreen.Width;
 
-            return zoomH == zoomW ? zoomH : 100;
+                return new Point(zoomW, zoomH);
+            }
+        }
+
+        public static PointF GetFontZoom
+        {
+            get
+            {
+                Size resolution = GetPhysicalDisplaySize;
+                Size virtualscreen = GetVirtualDisplaySize;
+                float zoomH = (resolution.Height * 100) / virtualscreen.Height;
+                float zoomW = (resolution.Width * 100) / virtualscreen.Width;
+
+                return new PointF(zoomW / 100, zoomH / 100);
+            }
         }
 
         public static PointF GetRealPoint(Point cursor)
         {
-            return new PointF(cursor.X * ((float)GetFontZoom() / 100),
-                cursor.Y * ((float)GetFontZoom() / 100));
+            return new PointF(cursor.X * GetFontZoom.X, cursor.Y * GetFontZoom.Y);
         }
 
         public static Point GetRoundedRealPoint(Point cursor)
         {
-            return new Point((int)Math.Round(cursor.X * ((float)GetFontZoom() / 100), 0),
-                (int)Math.Round(cursor.Y * ((float)GetFontZoom() / 100), 0));
+            return new Point((int)Math.Round(cursor.X * GetFontZoom.X, 0),
+                (int)Math.Round(cursor.Y * GetFontZoom.Y, 0));
         }
 
     }
